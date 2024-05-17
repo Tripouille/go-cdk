f8abebf6ae29229fbe3f87f6b486e3b0995b6174
jgambard <jmichel.gambard@gmail.com>
2024-05-17 15:08:55 +0200

    chore: setup api gateway

--- go-cdk.go
+++ go-cdk.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/aws/aws-cdk-go/awscdk/v2"
+	"github.com/aws/aws-cdk-go/awscdk/v2/awsapigateway"
 	"github.com/aws/aws-cdk-go/awscdk/v2/awsdynamodb"
 	"github.com/aws/aws-cdk-go/awscdk/v2/awslambda"
 	"github.com/aws/constructs-go/constructs/v10"
@@ -20,7 +21,7 @@ func NewGoCdkStack(scope constructs.Construct, id string, props *GoCdkStackProps
 
 	stack := awscdk.NewStack(scope, &id, &sprops)
 
-	createUserHandler := awslambda.NewFunction(stack, jsii.String("createUser"), &awslambda.FunctionProps{
+	userHandler := awslambda.NewFunction(stack, jsii.String("userHandler"), &awslambda.FunctionProps{
 		Runtime: awslambda.Runtime_PROVIDED_AL2023(),
 		Code:    awslambda.AssetCode_FromAsset(jsii.String("lambda/function.zip"), nil),
 		Handler: jsii.String("main"),
@@ -34,7 +35,26 @@ func NewGoCdkStack(scope constructs.Construct, id string, props *GoCdkStackProps
 		TableName: jsii.String("userTable"),
 	})
 
-	table.GrantReadWriteData(createUserHandler)
+	table.GrantReadWriteData(userHandler)
+
+	apiGateway := awsapigateway.NewRestApi(stack, jsii.String("apiGateway"), &awsapigateway.RestApiProps{
+		DefaultCorsPreflightOptions: &awsapigateway.CorsOptions{
+			AllowHeaders: jsii.Strings("Content-type", "Authorization"),
+			AllowMethods: jsii.Strings("GET", "POST"),
+			AllowOrigins: jsii.Strings("*"),
+		},
+		DeployOptions: &awsapigateway.StageOptions{
+			LoggingLevel: awsapigateway.MethodLoggingLevel_INFO,
+		},
+	})
+
+	userIntegration := awsapigateway.NewLambdaIntegration(userHandler, nil)
+
+	registerResource := apiGateway.Root().AddResource(jsii.String("register"), nil)
+	registerResource.AddMethod(jsii.String("POST"), userIntegration, nil)
+
+	loginResource := apiGateway.Root().AddResource(jsii.String("login"), nil)
+	loginResource.AddMethod(jsii.String("POST"), userIntegration, nil)
 
 	return stack
 }
